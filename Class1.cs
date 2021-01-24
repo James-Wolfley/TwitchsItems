d7dc0195f5b41ebddd24b61b9e1aabfb5a522e42
James Wolfley <wolfleyjames13@gmail.com>
2021-01-23 21:45:41 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace TwitchsItems
+{
+    [StaticConstructorOnStartup]
+    public static class TestMod
+    {
+        static TestMod()
+        {
+            Log.Message("Hello World!");
+        }
+    }
+}
